a6b16ceec2e16f9f62afb9a76dc664ab05e8a265
Jan Vennemann <jan.vennemann@gmx.net>
2016-04-23 02:11:55 +0200

    Add chalk to format console output

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import shuffleDeck from './dealer'
 import program from 'commander'
+import chalk from 'chalk'
 
 program
   .version('1.0.0')
@@ -9,5 +10,6 @@ program
   .option('-n, --cards <n>', 'Number of cards in the deck', parseInt)
   .parse(process.argv)
 
-var rounds = shuffleDeck(program.cards)
-console.log(rounds)
+console.log('Start shuffling of %s cards...', program.cards)
+const rounds = shuffleDeck(program.cards)
+console.log('It took ' + chalk.green.bold('%s') + ' rounds to return the cards into original order.', rounds)
