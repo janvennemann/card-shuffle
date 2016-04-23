646d94c546cf4aebf1b8bd1e0b867327b316be8a
Jan Vennemann <jan.vennemann@gmx.net>
2016-04-23 02:04:41 +0200

    Convert test specs to ES6 syntax

--- test/card-deck.spec.js
+++ test/card-deck.spec.js
@@ -1,78 +1,78 @@
-var chai = require('chai')
-var CardDeck = require('../lib/card-deck')
+import chai from 'chai'
+import CardDeck from '../card-deck'
 
-var should = chai.should()
+const should = chai.should()
 
 describe('Card Deck', function() {
-  var deck = null
+  let deck = null
 
-  var fillDeck = function (deck, numberOfCards) {
+  const fillDeck = (deck, numberOfCards) => {
       for (var i = 0; i < numberOfCards; i++) {
           deck.pushHead(i + 1);
       }
   }
 
-  beforeEach(function() {
+  beforeEach(() => {
     deck = new CardDeck()
   })
 
-  afterEach(function() {
+  afterEach(() => {
     deck = null
   })
 
-  it('should initiately contain zero items', function() {
+  it('should initiately contain zero items', () => {
     should.equal(0, deck.size)
     deck.isEmpty().should.equal(true)
   })
 
-  it('should return correct deck size', function() {
+  it('should return correct deck size', () => {
     should.equal(0, deck.size)
     fillDeck(deck, 3)
     should.equal(3, deck.size)
   })
 
-  it('should detect if empty or not', function() {
+  it('should detect if empty or not', () => {
     deck.isEmpty().should.equal(true)
     deck.pushHead(1)
     deck.isEmpty().should.equal(false)
   })
 
-  describe('adding cards', function() {
-    it('head and tail should be the same for first card', function() {
+  describe('adding cards', () => {
+    it('head and tail should be the same for first card', () => {
       deck.pushHead(1)
       deck.head.should.equal(deck.tail)
     })
 
-    it('should add card to the head of the deck', function() {
+    it('should add card to the head of the deck', () => {
       fillDeck(deck, 3)
       should.equal(3, deck.head.data)
     })
 
-    it('should add card to the tail of the deck', function() {
+    it('should add card to the tail of the deck', () => {
       fillDeck(deck, 3)
       deck.pushTail(0)
       should.equal(0, deck.tail.data)
     })
   })
 
-  describe('removing cards', function() {
-    it('should throw error if remove from empty deck', function() {
-      (function() {
+  describe('removing cards', () => {
+    it('should throw error if remove from empty deck', () => {
+      (() => {
         deck.popHead()
       }).should.Throw(Error, 'You can not pop from an empty stack')
     })
 
-    it('should remove card from the head of the deck', function() {
+    it('should remove card from the head of the deck', () => {
       fillDeck(deck, 6)
       deck.popHead().should.equal(6)
     })
 
-    it('should remove card from the tail of the deck', function() {
+    it('should remove card from the tail of the deck', () => {
       fillDeck(deck, 4)
       deck.popTail().should.equal(1)
     })
 
-    it('head and tail should be the same after removing all but one card', function() {
+    it('head and tail should be the same after removing all but one card', () => {
       fillDeck(deck, 3)
       deck.popHead()
       deck.popHead()
